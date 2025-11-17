1dc25ee48add4dab1a97929d631aefb23a6c6246
Majobran <Majobran2019@gmail.com>
2025-11-17 04:13:14 +0300

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import LoginForm from "./components/LoginForm";
+
+function App() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gradient-to-r from-purple-500 to-pink-500">
+      <LoginForm />
+    </div>
+  );
+}
+
+export default App;

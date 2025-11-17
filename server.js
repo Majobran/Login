f35dfb4dfc845215c59626c8af2a3112d6d47690
Majobran <Majobran2019@gmail.com>
2025-11-17 05:00:18 +0300

    Update server.js

--- server.js
+++ server.js
@@ -3,9 +3,12 @@ const path = require("path");
 const app = express();
 const port = process.env.PORT || 3000;
 
-// تقديم ملفات المشروع بشكل ثابت
-app.use(express.static(path.join(__dirname)));
+app.use(express.static(path.join(__dirname, "build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
 
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
+  console.log(`Server running on port ${port}`);
 });
